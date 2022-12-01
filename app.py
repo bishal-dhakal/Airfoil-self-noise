f924bc00ae852692dc79be5747f51a33506d84c1
Bishal Dhakal <46195146+bishal-dhakal@users.noreply.github.com>
2022-12-01 19:20:46 +0545

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import pickle
 import flask
-from  flask import Flask,request,app
+from  flask import Flask,request,app,jsonify
+import numpy as np
+import pandas as pd
 from flask import Response
 from flask_cors import CORS
 
 #start app
-app=Flask(__name__)  
-
+app=Flask(__name__)
+model=pickle.load(open('model.pkl', 'rb'))
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    
+    data=request.json['data']
+    print(data)
+    #to pass data as 2D array
+    new_data =[list(data.values())]
+    output=model.predict(new_data)[0]
+    return jsonify(output)
 
 if __name__=='__main__':
     app.run(debug=True)
